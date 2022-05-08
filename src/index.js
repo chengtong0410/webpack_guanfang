96de91ccc6fcfdb01fd64d1a4ac670d0bff590f0
chengTong <chengtong0410@163.com>
2022-05-08 23:39:59 +0800

    feat:修改3

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ function component() {
   btn.innerHTML = "点击这里，然后查看 console hahha";
   element.classList.add("hello");
   btn.onclick = function () {
-    console.log("我被点击了！2222222222233333333");
+    console.log("我被点击了！222222222223333333dasdaskdhaksd3");
   };
   element.appendChild(btn);
 
